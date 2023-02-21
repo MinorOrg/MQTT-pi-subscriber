30ed00d7cc0e24e2665cd650268b107dd433d6ac
Rujal Acharya <076bei029.rujal@pcampus.edu.np>
2023-02-21 11:00:16 +0545

    Refactor

--- subscriber.py
+++ subscriber.py
@@ -3,6 +3,7 @@ import paho.mqtt.client as mqtt
 MQTT_ADDRESS = '192.168.43.134'
 MQTT_USER = 'mosquitong'
 MQTT_PASSWORD = 'elong-mosquitong'
+MQTT_TOPIC_ESP = 'esp/+'
 MQTT_TOPIC_ESP1 = 'esp/rec1'
 MQTT_TOPIC_ESP2 = 'esp/rec2'
 MQTT_TOPIC_ESP3 = 'esp/rec3'
@@ -12,58 +13,44 @@ esp2data = {}
 esp3data = {}
 
 
-
 def on_connect(client, userdata, flags, rc):
     print('Connected with result code ' + str(rc))
-    client.subscribe('esp/+')
-    
+    client.subscribe(MQTT_TOPIC_ESP)
+
 
 def on_message(client, userdata, msg):
     payload = str(msg.payload)[2::][::-1][1::][::-1]
     for wifi in payload.split(';'):
         if wifi == ' ':
             continue
-        # print(wifi)
         ssidrssi = str(wifi).split(':')
-        # print(ssidrssi)
         ssid = ssidrssi[0].strip()
         rssi = int(ssidrssi[1].strip())
-        if msg.topic == 'esp/rec1':
-            
+        if msg.topic == MQTT_TOPIC_ESP1:
             esp1data[ssid] = rssi
-            # print(esp1data)
             if ssid == "wifi":
                 print("esp1:", end=" ")
                 print(rssi)
-        elif msg.topic == 'esp/rec2':
+        elif msg.topic == MQTT_TOPIC_ESP2:
             esp2data[ssid] = rssi
             if ssid == "wifi":
                 print("esp2:", end=" ")
                 print(rssi)
-        elif msg.topic == 'esp/rec3':
+        elif msg.topic == MQTT_TOPIC_ESP3:
             esp3data[ssid] = rssi
             if ssid == "wifi":
                 print("esp3:", end=" ")
                 print(rssi)
 
 
-
-            
-
-
- 
-
-def main(): 
+def main():
     mqtt_client = mqtt.Client()
- 
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
     mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
-
     mqtt_client.connect(MQTT_ADDRESS, 1883, 60)
     mqtt_client.loop_forever()
 
- 
 
 if __name__ == '__main__':
     main()
