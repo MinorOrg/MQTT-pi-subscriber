aaf61f12a3994153f077e0b00c30d7d55ec72eb7
Rujal Acharya <076bei029.rujal@pcampus.edu.np>
2023-03-26 12:48:22 +0000

    fix the loop problem

--- display-rt.py
+++ display-rt.py
@@ -12,13 +12,11 @@ MQTT_TOPIC_ESP3 = 'esp/ble3'
 
 testssid = "wifi"
 
-esp1rssi_nothing = 2
-esp2rssi_nothing = 2
-esp3rssi_nothing = 2
 
-esp1rssi_mobhaile = 1
-esp2rssi_mobhaile = 3
-esp3rssi_mobhaile = 2
+
+
+pygame.init()
+
 
 size = (1400, 700)
 radiationSurfaceSize = (700, 700)
@@ -42,15 +40,38 @@ esp1Radiation = pygame.Color(192, 57, 43, 128)
 esp2Radiation = pygame.Color(41, 128, 185,128)
 esp3Radiation = pygame.Color(142, 68, 173,128)
 
+font = pygame.font.Font(None, 24)
+
+device1Text = font.render("Device 1", True, white)
+device2Text = font.render("Device 2", True, white)
+
+
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Trilateration Visualizer")
+
 A = [-92.6483870967742, -87.7368191721133, -90.50672043010752]
 
 n = 2
+
+running = True
 
 def on_connect(client, userdata, flags, rc):
     print('Connected with result code ' + str(rc))
     client.subscribe(MQTT_TOPIC_ESP)
 
+esp1rssi_nothing = 0
+esp2rssi_nothing = 0
+esp3rssi_nothing = 0
+
+esp1rssi_mobhaile = 0
+esp2rssi_mobhaile = 0
+esp3rssi_mobhaile = 0
+
 def on_message(client, userdata, msg):
+
+    global esp1rssi_nothing, esp1rssi_mobhaile, esp2rssi_nothing, esp2rssi_mobhaile, esp3rssi_nothing, esp3rssi_mobhaile
+
+
     payload = str(msg.payload)[2::][::-1][1::][::-1].strip()
     print(payload)
     for ble in payload.split(':'):
@@ -80,29 +101,11 @@ def on_message(client, userdata, msg):
             elif name == "mobhaile":
                 esp3rssi_mobhaile = 10**((A[2]-rssi)/(10*n))
                 # mobhaile_file.write(f"esp3: {rssi} \n")
-
-def clearScreen(screen):
-    screen.fill("black")
-
-def main():
-    pygame.init()
-
-    mqtt_client = mqtt.Client()
-    mqtt_client.on_connect = on_connect
-    mqtt_client.on_message = on_message
-    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
-    mqtt_client.connect(MQTT_ADDRESS, 1883, 60)
-    mqtt_client.loop_forever()
-
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("Trilateration Visualizer")
-
-    # Font for the text
-    font = pygame.font.Font(None, 24)
-
-    device1Text = font.render("Device 1", True, white)
-    device2Text = font.render("Device 2", True, white)
-
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
     esp1Text_1 = font.render("ESP1: " + str(round(esp1rssi_nothing, 2)) + "m", True, (255, 255, 255))
     esp2Text_1 = font.render("ESP2: " + str(round(esp2rssi_nothing, 2)) + "m", True, (255, 255, 255))
     esp3Text_1 = font.render("ESP3: " + str(round(esp3rssi_nothing, 2)) + "m", True, (255, 255, 255))
@@ -135,75 +138,73 @@ def main():
     esp2Rect_2.topleft = (710, esp1Rect_2.bottomleft[1] + 5)
     esp3Rect_2.topleft = (710, esp2Rect_2.bottomleft[1] + 5)
 
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        
-        
-        clearScreen(screen)
+    # if running:    
+    clearScreen(screen)
+    # Surface to draw the circles
+    radiationSurface1_1 = pygame.Surface(radiationSurfaceSize, pygame.SRCALPHA)
+    radiationSurface2_1 = pygame.Surface(radiationSurfaceSize, pygame.SRCALPHA)
+    radiationSurface3_1 = pygame.Surface(radiationSurfaceSize, pygame.SRCALPHA)
+    radiationSurface1_2 = pygame.Surface(radiationSurfaceSize, pygame.SRCALPHA)
+    radiationSurface2_2 = pygame.Surface(radiationSurfaceSize, pygame.SRCALPHA)
+    radiationSurface3_2 = pygame.Surface(radiationSurfaceSize, pygame.SRCALPHA)
+    # Draw a circle around the esp devices with a breathing pattern
+    radius1_1 = esp1rssi_nothing * 200
+    radius2_1 = esp2rssi_nothing * 200
+    radius3_1 = esp3rssi_nothing * 200
+    radius1_2 = esp1rssi_mobhaile * 200
+    radius2_2 = esp2rssi_mobhaile * 200
+    radius3_2 = esp3rssi_mobhaile * 200
+    # Draw a circle around the esp devices
+    pygame.draw.circle(radiationSurface1_1, esp1Radiation, esp1_1, radius1_1)
+    pygame.draw.circle(radiationSurface2_1, esp2Radiation, esp2_1, radius2_1)
+    pygame.draw.circle(radiationSurface3_1, esp3Radiation, esp3_1, radius3_1)
+    pygame.draw.circle(radiationSurface1_2, esp1Radiation, esp1_1, radius1_2)
+    pygame.draw.circle(radiationSurface2_2, esp2Radiation, esp2_1, radius2_2)
+    pygame.draw.circle(radiationSurface3_2, esp3Radiation, esp3_1, radius3_2)
+    # Draw the surfaces to the screen
+    screen.blit(radiationSurface1_1, (0,0))
+    screen.blit(radiationSurface2_1, (0,0))
+    screen.blit(radiationSurface3_1, (0,0))
+    screen.blit(radiationSurface1_2, (700,0))
+    screen.blit(radiationSurface2_2, (700,0))
+    screen.blit(radiationSurface3_2, (700,0))
+    # Display esps for first phone
+    pygame.draw.circle(screen, esp1Color, esp1_1, 5)
+    pygame.draw.circle(screen, esp2Color, esp2_1, 5)
+    pygame.draw.circle(screen, esp3Color, esp3_1, 5)
+    # Draw line between the screens
+    pygame.draw.line(screen, white, (700, 0), (700, 700))
+    # Display esps for second phone
+    pygame.draw.circle(screen, esp1Color, esp1_2, 5)
+    pygame.draw.circle(screen, esp2Color, esp2_2, 5)
+    pygame.draw.circle(screen, esp3Color, esp3_2, 5)
+    # Display the text
+    screen.blit(device1Text, device1Rect)
+    screen.blit(device2Text, device2Rect)
+    screen.blit(esp1Text_1, esp1Rect_1)
+    screen.blit(esp2Text_1, esp2Rect_1)
+    screen.blit(esp3Text_1, esp3Rect_1)
+    screen.blit(esp1Text_2, esp1Rect_2)
+    screen.blit(esp2Text_2, esp2Rect_2)
+    screen.blit(esp3Text_2, esp3Rect_2)
+    pygame.display.flip()
 
-        # Surface to draw the circles
-        radiationSurface1_1 = pygame.Surface(radiationSurfaceSize, pygame.SRCALPHA)
-        radiationSurface2_1 = pygame.Surface(radiationSurfaceSize, pygame.SRCALPHA)
-        radiationSurface3_1 = pygame.Surface(radiationSurfaceSize, pygame.SRCALPHA)
+def clearScreen(screen):
+    screen.fill("black")
 
-        radiationSurface1_2 = pygame.Surface(radiationSurfaceSize, pygame.SRCALPHA)
-        radiationSurface2_2 = pygame.Surface(radiationSurfaceSize, pygame.SRCALPHA)
-        radiationSurface3_2 = pygame.Surface(radiationSurfaceSize, pygame.SRCALPHA)
+def main():
+   
 
-        # Draw a circle around the esp devices with a breathing pattern
-        radius1_1 = esp1rssi_nothing * 200
-        radius2_1 = esp2rssi_nothing * 200
-        radius3_1 = esp3rssi_nothing * 200
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+    mqtt_client.connect(MQTT_ADDRESS, 1883, 60)
+    mqtt_client.loop_forever()
 
-        radius1_2 = esp1rssi_mobhaile * 200
-        radius2_2 = esp2rssi_mobhaile * 200
-        radius3_2 = esp3rssi_mobhaile * 200
+    
 
-        # Draw a circle around the esp devices
-        pygame.draw.circle(radiationSurface1_1, esp1Radiation, esp1_1, radius1_1)
-        pygame.draw.circle(radiationSurface2_1, esp2Radiation, esp2_1, radius2_1)
-        pygame.draw.circle(radiationSurface3_1, esp3Radiation, esp3_1, radius3_1)
-
-        pygame.draw.circle(radiationSurface1_2, esp1Radiation, esp1_1, radius1_2)
-        pygame.draw.circle(radiationSurface2_2, esp2Radiation, esp2_1, radius2_2)
-        pygame.draw.circle(radiationSurface3_2, esp3Radiation, esp3_1, radius3_2)
-
-        # Draw the surfaces to the screen
-        screen.blit(radiationSurface1_1, (0,0))
-        screen.blit(radiationSurface2_1, (0,0))
-        screen.blit(radiationSurface3_1, (0,0))
-
-        screen.blit(radiationSurface1_2, (700,0))
-        screen.blit(radiationSurface2_2, (700,0))
-        screen.blit(radiationSurface3_2, (700,0))
-
-        # Display esps for first phone
-        pygame.draw.circle(screen, esp1Color, esp1_1, 5)
-        pygame.draw.circle(screen, esp2Color, esp2_1, 5)
-        pygame.draw.circle(screen, esp3Color, esp3_1, 5)
-
-        # Draw line between the screens
-        pygame.draw.line(screen, white, (700, 0), (700, 700))
-
-        # Display esps for second phone
-        pygame.draw.circle(screen, esp1Color, esp1_2, 5)
-        pygame.draw.circle(screen, esp2Color, esp2_2, 5)
-        pygame.draw.circle(screen, esp3Color, esp3_2, 5)
-
-        # Display the text
-        screen.blit(device1Text, device1Rect)
-        screen.blit(device2Text, device2Rect)
-        screen.blit(esp1Text_1, esp1Rect_1)
-        screen.blit(esp2Text_1, esp2Rect_1)
-        screen.blit(esp3Text_1, esp3Rect_1)
-        screen.blit(esp1Text_2, esp1Rect_2)
-        screen.blit(esp2Text_2, esp2Rect_2)
-        screen.blit(esp3Text_2, esp3Rect_2)
-
-        pygame.display.flip()
-
+    # Font for
+       
 if __name__ == '__main__':
     main()
